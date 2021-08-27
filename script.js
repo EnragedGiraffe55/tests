fd5561beaeb9d891d82401f3146933b89ed775ff
Giraffeboi5 <89265404+EnragedGiraffe55@users.noreply.github.com>
2021-08-27 16:39:11 +0000

    aaa

--- script.js
+++ script.js
@@ -5,18 +5,20 @@ const gameState = {
 function preload()
 {
   this.load.image('test1','assets/yeet.png');
-  this.load.image('test2','assets/yeet.png');
-  this.load.spritesheet('run','assets/run.png', { frameWidth: 72, frameHeight: 90 })
+  this.load.spritesheet('run2','assets/run.png', { frameWidth: 72, frameHeight: 90 });
+  this.load.spritesheet('run','assets/run2.png', { frameWidth: 240, frameHeight: 450 });
 }
 
 function create()
 {
-gameState.active = true
+gameState.active = true;
 
 gameState.player = this.physics.add.sprite(100,100,'test1');
-gameState.run = this.physics.add.sprite(200,200,'run');
+//gameState.run = this.physics.add.sprite(200,200,'run');
+gameState.run2 = this.physics.add.sprite(150,150,'run2');
 
 gameState.player.setScale(1);
+gameState.run2.setScale(.2);
 
 gameState.cursors = this.input.keyboard.createCursorKeys();
 gameState.cursors.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -26,8 +28,8 @@ gameState.cursors.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCod
 
 this.anims.create({
   key: 'run',
-  frames: this.anims.generateFrameNumbers('run', { start: 0, end: 3 }),
-      frameRate: 5,
+  frames: this.anims.generateFrameNumbers('run', { start: 0, end: 2 }),
+      frameRate: 3,
       repeat: -1
 })
 }
@@ -37,24 +39,24 @@ function update()
 {
 if (gameState.active){
 if (gameState.cursors.keyA.isDown) {
-    gameState.run.setVelocityX(-160);
-    gameState.run.anims.play('run',true);
-    gameState.run.flipX = true;
+    gameState.run2.setVelocityX(-160);
+    gameState.run2.anims.play('run',true);
+    gameState.run2.flipX = true;
   } else if (gameState.cursors.keyD.isDown) {
-    gameState.run.setVelocityX(160);
-    gameState.run.anims.play('run',true);
-    gameState.run.flipX = false;
+    gameState.run2.setVelocityX(160);
+    gameState.run2.anims.play('run',true);
+    gameState.run2.flipX = false;
   } else {
-    gameState.run.setVelocityX(0);
-    gameState.run.anims.play('run',false);
+    gameState.run2.setVelocityX(0);
+    gameState.run2.anims.play('run',false);
   }
 
 if (gameState.cursors.keyW.isDown) {
-    gameState.run.setVelocityY(-160);
+    gameState.run2.setVelocityY(-160);
   } else if (gameState.cursors.keyS.isDown) {
-    gameState.run.setVelocityY(160);
+    gameState.run2.setVelocityY(160);
   } else {
-    gameState.run.setVelocityY(0);
+    gameState.run2.setVelocityY(0);
   }
 }
 
