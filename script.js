d3accf70145c3399dd3a579b868e2a5dd418da6d
Giraffeboi5 <89265404+EnragedGiraffe55@users.noreply.github.com>
2021-09-01 16:38:17 +0000

    nn

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function preload()
 {
   this.load.image('test1','assets/yeet.png');
   this.load.image('enemy','assets/zombie1.png');
-  this.load.spritesheet('run','assets/run2.png', { frameWidth: 50, frameHeight: 100 });
+  this.load.spritesheet('run','assets/run2.png', { frameWidth: 37, frameHeight: 69 });
 }
 
 function create()
@@ -17,12 +17,15 @@ gameState.active = true;
 gameState.enemy = this.physics.add.sprite(100,100,'enemy').setScale(.8);
 gameState.run2 = this.physics.add.sprite(150,150,'run2');
 
-this.physics.add.collider(gameState.run2, gameState.enemy)
+this.physics.add.collider(gameState.run2, gameState.enemy, function (){
+  
+})
 gameState.enemy.setCollideWorldBounds(true);
 gameState.run2.setCollideWorldBounds(true);
 
 //gameState.player.setScale(1);
 gameState.run2.setScale(1);
+gameState.run2.setSize(37,69,0);
 
 gameState.cursors = this.input.keyboard.createCursorKeys();
 gameState.cursors.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -32,8 +35,8 @@ gameState.cursors.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCod
 
 this.anims.create({
   key: 'run',
-  frames: this.anims.generateFrameNumbers('run', { start: 0, end: 2 }),
-      frameRate: 3.5,
+  frames: this.anims.generateFrameNumbers('run', { start: 0, end: 2}),
+      frameRate: 3,
       repeat: -1
 })
 }
